6e405bed677b017f15ef4fba96643b4d82243f89
Rrohitkg <rohit1998kg@gmail.com>
2021-05-29 23:07:58 +0530

    Edited hello world and added print

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@ int main()
 {
     printf("Welcome to Github Actions\n");
     printf("\n*******Hurry, It works******\n");
-    printf("Yes, it realy works. Nice work!!");
+    printf("Yes, it really works. Nice work!!");
     return 0;
 }
 
